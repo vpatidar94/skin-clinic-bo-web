9c8aaf162c11f53a8cd82fbbaab097417188c2e4
Vinay Patidar <vpatidar94@gmail.com>
2023-07-28 13:18:38 +0530

    Change vo's

--- src/app/@shared/component/address/address.module.ts
+++ src/app/@shared/component/address/address.module.ts
@@ -2,9 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { LatLngModule } from '../lat-lng/lat-lng.module';
 import { EditAddressFormComponent } from './edit-address-form.component';
-import { EditAddressComponent } from './edit-address.component';
 import { ViewAddressComponent } from './view-address.component';
 import { MatInputModule } from '@angular/material/input';
 import { SharedDirectiveModule } from '../../directive/shared-directive.module';
@@ -13,9 +11,9 @@ import { SharedDirectiveModule } from '../../directive/shared-directive.module';
 const COMMON_MODULE = [CommonModule, FormsModule];
 
 @NgModule({
-  imports: [...COMMON_MODULE, LatLngModule, MatFormFieldModule, MatInputModule, SharedDirectiveModule],
-  exports: [ViewAddressComponent, EditAddressComponent, EditAddressFormComponent], // declare view and edit comp as public
-  declarations: [ViewAddressComponent, EditAddressComponent, EditAddressFormComponent]
+  imports: [...COMMON_MODULE, MatFormFieldModule, MatInputModule, SharedDirectiveModule],
+  exports: [ViewAddressComponent, EditAddressFormComponent], // declare view and edit comp as public
+  declarations: [ViewAddressComponent, EditAddressFormComponent]
 })
 export class AddressModule {
 }
