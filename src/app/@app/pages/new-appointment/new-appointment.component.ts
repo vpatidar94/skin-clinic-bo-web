37dcd80200116a8d194f6868a92ce21ea1b9b34e
Jitendra Yadav <111629790+yadavjit@users.noreply.github.com>
2023-09-26 12:51:24 +0530

    some change in billing component and some change in new appointment component

--- src/app/@app/pages/new-appointment/new-appointment.component.ts
+++ src/app/@app/pages/new-appointment/new-appointment.component.ts
@@ -1,15 +1,17 @@
 import { NgForm } from '@angular/forms';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
-import { MatSort} from '@angular/material/sort';
+import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { KeyValueStorageService } from 'src/app/@shared/service/key-value-storage.service';
 import { ServiceItemApi } from '../../service/remote/service-item.api';
-import { ApiResponse, BookingVo, ItemDetailDto, ObservationVo, ResponseStatus, UserBookingDto, UserVo, BOOKING_TYPE, BOOKING_TYPE_NAME, KeyValueVo, PrescriptionVo, AddressVo, OrgBookingDto, ProductVo, } from 'aayam-clinic-core';
+import { ApiResponse, BookingVo, ItemDetailDto, ObservationVo, ResponseStatus, UserBookingDto, UserVo, BOOKING_TYPE, BOOKING_TYPE_NAME, KeyValueVo, PrescriptionVo, AddressVo, OrgBookingDto, ProductVo, OrgBookingCountDto, } from 'aayam-clinic-core';
 import { UserApi } from '../../service/remote/user.api';
 import { SUB_ROLE } from '../../const/sub-role.const';
 import { BookingApi } from '../../service/remote/booking.api';
+import { catchError, map, of as observableOf, startWith, switchMap } from 'rxjs';
+
 
 export interface PeriodicElement {
   appNo: string;
@@ -35,12 +37,12 @@ export interface ColumnWiseFiltersDto {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { appNo: "1", name: 'Rahul Dongre', date: '09/6/2023', time: '12:00-12:10', doctor: "Dr Ramesh Mahajan", consultationFor:"normal checkup", action: "add appointment | delete", },
-  { appNo: "2", name: 'Abhay Singh', date: '09/6/2023', time: '11:30-11:40', doctor: "Dr Ram Shrivastava", consultationFor:"normal checkup", action: "add appointment | delete", },
-  { appNo: "3", name: 'Sunny Thakur', date: '12/6/2023', time: '01:00-01:10', doctor: "Dr Mayank Patidar", consultationFor:"normal checkup", action: "add appointment | delete", },
-  { appNo: "4", name: 'Vishal Pandit', date: '15/6/2023', time: '03:00-03:10', doctor: "Dr Mayur Patidar", consultationFor:"normal checkup", action: "add appointment | delete", },
-  { appNo: "5", name: 'Rahul Dongre', date: '09/6/2023', time: '12:00-12:10', doctor: "Dr Ramesh Mahajan", consultationFor:"normal checkup", action: "add appointment | delete", },
-  { appNo: "6", name: 'Rahul Dongre', date: '15/6/2023', time: '12:00-12:10', doctor: "Dr Ramesh Mahajan", consultationFor:"normal checkup", action: "add appointment | delete", },
+  { appNo: "1", name: 'Rahul Dongre', date: '09/6/2023', time: '12:00-12:10', doctor: "Dr Ramesh Mahajan", consultationFor: "normal checkup", action: "add appointment | delete", },
+  { appNo: "2", name: 'Abhay Singh', date: '09/6/2023', time: '11:30-11:40', doctor: "Dr Ram Shrivastava", consultationFor: "normal checkup", action: "add appointment | delete", },
+  { appNo: "3", name: 'Sunny Thakur', date: '12/6/2023', time: '01:00-01:10', doctor: "Dr Mayank Patidar", consultationFor: "normal checkup", action: "add appointment | delete", },
+  { appNo: "4", name: 'Vishal Pandit', date: '15/6/2023', time: '03:00-03:10', doctor: "Dr Mayur Patidar", consultationFor: "normal checkup", action: "add appointment | delete", },
+  { appNo: "5", name: 'Rahul Dongre', date: '09/6/2023', time: '12:00-12:10', doctor: "Dr Ramesh Mahajan", consultationFor: "normal checkup", action: "add appointment | delete", },
+  { appNo: "6", name: 'Rahul Dongre', date: '15/6/2023', time: '12:00-12:10', doctor: "Dr Ramesh Mahajan", consultationFor: "normal checkup", action: "add appointment | delete", },
 
 ]
 
@@ -50,7 +52,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./new-appointment.component.scss']
 })
 
-export class NewAppointmentComponent implements OnInit {
+export class NewAppointmentComponent implements OnInit, AfterViewInit {
 
   showSectionAppointmentList: boolean = false;
   showSectionAppointmentEdit: boolean = false;
@@ -58,7 +60,7 @@ export class NewAppointmentComponent implements OnInit {
 
   userBooking!: UserBookingDto;
 
-  orgBooking! : OrgBookingDto;
+  orgBooking!: OrgBookingDto;
 
   resultsLength = 0;
   serviceItemList!: ItemDetailDto[];
@@ -68,7 +70,7 @@ export class NewAppointmentComponent implements OnInit {
 
   productList!: ProductVo[];
 
-  displayedColumns: string[] = ['appNo', 'name', 'date', "time", "doctor", "consultationFor","action"];
+  displayedColumns: string[] = ['appNo', 'name', 'date', "time", "doctor", "consultationFor", "action"];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -91,12 +93,14 @@ export class NewAppointmentComponent implements OnInit {
 
 
   filteredData: PeriodicElement[] = [...ELEMENT_DATA];
+
+  bookingTypeName: any = BOOKING_TYPE_NAME;
   /* ************************************ Constructors ************************************ */
   constructor(private keyValueStorageService: KeyValueStorageService,
     private serviceItemApi: ServiceItemApi,
     private userApi: UserApi,
     private bookingApi: BookingApi,
-    ) {
+  ) {
   }
 
   /* ************************************ Public Methods ************************************ */
@@ -117,7 +121,7 @@ export class NewAppointmentComponent implements OnInit {
   //   const filterValue = (event.target as HTMLInputElement).value;
   //   this.filters[columnName] = filterValue.trim().toLowerCase();
   //   this.dataSource.filter = JSON.stringify(this.filters);
-  
+
   //   if (this.dataSource.paginator) {
   //     this.dataSource.paginator.firstPage();
   //   }
@@ -125,35 +129,35 @@ export class NewAppointmentComponent implements OnInit {
 
   public applyOldFilter(event: Event, columnName: string): void {
     const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
-    
+
     // Create a custom filter function based on the selected column
     const filterFunction = (data: PeriodicElement): boolean => {
       // Use the selected column to access the corresponding property in data
       const columnValue = data[columnName].toLowerCase();
       return columnValue.includes(filterValue);
     };
-  
+
     // Set the custom filter function to the data source filter predicate
     this.dataSource.filterPredicate = filterFunction;
-  
+
     // Apply the filter
     this.dataSource.filter = filterValue;
   }
 
   public applyNewFilter(event: Event, columnName: string): void {
     const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
-  
+
     // Filter data based on the current column
     const filteredColumnData = this.filteredData.filter((data) =>
       data[columnName].toLowerCase().includes(filterValue)
     );
-  
+
     // Update the filteredData array with the filteredColumnData
     this.filteredData = filteredColumnData;
-  
+
     // Update the table data source
     this.dataSource.data = this.filteredData;
-  
+
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
@@ -192,8 +196,8 @@ export class NewAppointmentComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  public searchPreviousAppointments():void {
-    
+  public searchPreviousAppointments(): void {
+
 
   }
 
@@ -210,10 +214,51 @@ export class NewAppointmentComponent implements OnInit {
     this._init();
   }
 
-public getAllFilters():void {
-this.showAllFilters = !this.showAllFilters;
-}
-  
+  public getAllFilters(): void {
+    this.showAllFilters = !this.showAllFilters;
+  }
+
+  // newly added
+  public getBookingType(type: string): string {
+    if (!type) {
+      return '';
+    }
+    return this.bookingTypeName[type] as string;
+  }
+
+  public ngAfterViewInit() {
+    const orgId = this.keyValueStorageService.getOrgId();
+    if (!orgId) {
+      return;
+    }
+    this.dataSource.paginator = this.paginator;
+
+    this.paginator.page
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          return this.bookingApi.getOrgBookingList(
+            orgId,
+            BOOKING_TYPE.APPOINTMENT,
+            this.paginator.pageIndex + 1,
+            this.paginator.pageSize
+          ).pipe(catchError(() => observableOf()));
+        }),
+        map((res: ApiResponse<OrgBookingCountDto>) => {
+          if (res.body) {
+            this.resultsLength = res.body?.totalBooking;
+            return res.body;
+          }
+          return {} as OrgBookingCountDto;
+        })
+      )
+      .subscribe((dto) => {
+        this.bookingList = dto?.orgBooking ?? [] as OrgBookingDto[];
+        // this.dataSource = new MatTableDataSource(this.bookingList);
+      });
+
+  }
+
 
   /* ************************************ Private Methods ************************************ */
   private _init(): void {
@@ -223,10 +268,12 @@ this.showAllFilters = !this.showAllFilters;
     this._getDoctorList();
 
   }
+
   private _resetSection(): void {
     this.showSectionAppointmentList = false;
     this.showSectionAppointmentEdit = false;
   }
+
   private _addEditAppointment(userBooking: UserBookingDto): void {
     this.userBooking = userBooking;
     // console.log("xxxxshow",this.userBooking);
@@ -234,16 +281,14 @@ this.showAllFilters = !this.showAllFilters;
     this.showSectionAppointmentEdit = true;
   }
 
-  
-
   private _getServiceList(): void {
     const orgId = this.keyValueStorageService.getOrgId();
     if (!orgId) {
       return;
     }
     this.serviceItemApi.getServiceItemList(orgId).subscribe((res: ApiResponse<ItemDetailDto[]>) => {
-        if (res.body && res.body?.length > 0) {
-          this.serviceItemList = res.body;
+      if (res.body && res.body?.length > 0) {
+        this.serviceItemList = res.body;
       }
     });
   }
@@ -254,10 +299,10 @@ this.showAllFilters = !this.showAllFilters;
       return;
     }
     this.userApi.getDoctorList(orgId, SUB_ROLE.DOCTOR).subscribe((res: ApiResponse<UserVo[]>) => {
-        if (res.body && res.body?.length > 0) {
-          this.doctorList = res.body;
-        }
+      if (res.body && res.body?.length > 0) {
+        this.doctorList = res.body;
       }
+    }
     );
   }
 
