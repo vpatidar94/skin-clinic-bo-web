7478c5479247429fb57cca66b34f8adddc3e5037
Jitendra Yadav <111629790+yadavjit@users.noreply.github.com>
2023-09-27 17:01:28 +0530

    changes in the design of receipt

--- src/app/receipt/receipt.component.ts
+++ src/app/receipt/receipt.component.ts
@@ -9,23 +9,24 @@ export class ReceiptComponent {
   currentDate = new Date();
   receiptNo = '001223'
   doctorName = 'Dr. John Snow';
-  patientId ='1214';
+  patientId = '1214';
+  visitId = '0125'
   patientName = 'Mr Sheru Verma';
   fatherName = 'Mr Singh';
-  gender = '';
-  age = '';
-  patientAddress= '';
-  paymentMode='';
-  paymentType='';
+  gender = 'Male';
+  age = '29';
+  patientAddress = '';
+  paymentMode = 'Cash';
+  paymentStatus = "Paid";
 
   services = [
-    { name: 'Consultation', price: 100},
-    { name: 'X-Ray', price: 150 },
-    { name: 'Blood Test', price: 75.50 },
-    { name: 'PRP', price: 750 },
+    { name: 'Consultation', price: 100, qty: 1 },
+    { name: 'X-Ray', price: 150, qty: 1 },
+    { name: 'Blood Test', price: 75, qty: 1 },
+    { name: 'PRP', price: 750, qty: 1 },
   ];
 
   calculateTotal(): number {
-    return this.services.reduce((total, service) => total + service.price, 0);
+    return this.services.reduce((total, service) => total + ((service.price) * (service.qty)), 0);
   }
 }
