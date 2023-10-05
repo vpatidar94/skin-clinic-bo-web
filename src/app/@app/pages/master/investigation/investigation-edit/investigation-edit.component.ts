f88ff3e4520640bf7ce63d7ab7b85ae5643baf5c
Jitendra Yadav <111629790+yadavjit@users.noreply.github.com>
2023-10-05 19:43:51 +0530

    add btn and delete btn made working in Master/investigation component

--- src/app/@app/pages/master/investigation/investigation-edit/investigation-edit.component.ts
+++ src/app/@app/pages/master/investigation/investigation-edit/investigation-edit.component.ts
@@ -1,19 +1,49 @@
-import { Component, Input, } from '@angular/core';
+import { Component, Input, OnInit, } from '@angular/core';
 import { ProductVo } from 'aayam-clinic-core';
+
+export interface TestParameterInterface {
+    testName: string;
+    reference: string;
+    unit:string;
+    
+  }
 
 @Component({
     selector: 'app-investigation-edit',
     templateUrl: './investigation-edit.component.html',
 })
-export class InvestigationEditComponent {
+export class InvestigationEditComponent implements OnInit{
     /* ********************************* Static Field *************************************** */
     /* *********************************** Instance Field *********************************** */
     
     /* ************************************* Constructors ******************************************** */
     constructor() { }
 
+    testParameterData = [{
+        testName: '',
+        reference: '',
+        unit:'',
+      }];
+ 
+    testParameter!:Array<TestParameterInterface>
+    
     /* ************************************* Public Methods ******************************************** */
    
     /* ************************************* Private Methods ******************************************** */
+public ngOnInit(): void {
+ this.testParameter= (this.testParameterData as Array<TestParameterInterface>);
+}
+
+    public removeTestParameter(index: number): void {
+        this.testParameter.splice(index, 1);
+    }
+
+    public addTestParameter() {
+        this.testParameter.push({
+            testName: "",
+            reference: "",
+            unit:"",
+        });
+    }
 
 }
