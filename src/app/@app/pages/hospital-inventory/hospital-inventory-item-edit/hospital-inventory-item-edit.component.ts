85d3533dbbea2d255ca621def41eb46ac605e3ad
Jitendra Yadav <111629790+yadavjit@users.noreply.github.com>
2023-10-26 12:09:35 +0530

    added add and delete btn in the add item form of hospital inventory

--- src/app/@app/pages/hospital-inventory/hospital-inventory-item-edit/hospital-inventory-item-edit.component.ts
+++ src/app/@app/pages/hospital-inventory/hospital-inventory-item-edit/hospital-inventory-item-edit.component.ts
@@ -1,16 +1,56 @@
-import { Component, Input, } from '@angular/core';
+import { Component, Input, OnInit, } from '@angular/core';
 import { ProductVo } from 'aayam-clinic-core';
+
+export interface NewItemInterface {
+    sNo: string,
+    itemName: string,
+    company: string,
+    qty: string,
+    packaging: string,
+    expiry: string,
+}
 
 @Component({
     selector: 'app-hospital-inventory-item-edit',
     templateUrl: './hospital-inventory-item-edit.component.html',
 })
-export class HospitalInventoryItemEditComponent {
+export class HospitalInventoryItemEditComponent implements OnInit{
+
+    newItemData = [{
+        sNo: '',
+        itemName: '',
+        company: '',
+        qty: '',
+        packaging: '',
+        expiry: '',
+    }];
+
+    newItem!: Array<NewItemInterface>
+
     /* ********************************* Static Field *************************************** */
     /* *********************************** Instance Field *********************************** */
 
     /* ************************************* Constructors ******************************************** */
     constructor() { }
+
+    public ngOnInit(): void {
+        this.newItem= (this.newItemData as Array<NewItemInterface>);
+       }
+
+    public removeTestParameter(index: number): void {
+        this.newItem.splice(index, 1);
+    }
+
+    public addTestParameter() {
+        this.newItem.push({
+            sNo: '',
+            itemName: '',
+            company: '',
+            qty: '',
+            packaging: '',
+            expiry: '',
+        });
+    }
 
     /* ************************************* Public Methods ******************************************** */
 
